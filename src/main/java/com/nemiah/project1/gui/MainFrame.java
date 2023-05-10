629526d34bae062e3e621a204829c8ebe4e179e9
NemiahHayes <104559735+NemiahHayes@users.noreply.github.com>
2023-05-10 20:05:57 +1200

    Further Code Optimizations, Completed Startup

--- src/main/java/com/nemiah/project1/gui/MainFrame.java
+++ src/main/java/com/nemiah/project1/gui/MainFrame.java
@@ -5,9 +5,16 @@
 package com.nemiah.project1.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
 
 /**
@@ -18,27 +25,35 @@ public class MainFrame {
     
     private final int WINDOW_WIDTH = 400;
     private final int WINDOW_HEIGHT = 800;
+    public static JFrame frame;
     
     public MainFrame(){
         initialize();
     }
     
-    public void initialize(){
+    public void update(){
+        frame.repaint();
+    }
+    
+    private void initialize(){
         
         String windowTitle = "Dungeon Pet Simulator";
         
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setTitle(windowTitle);   
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
-        
-        JPanel mainPanel = new JPanel(new MigLayout());
-        
-        mainPanel.add(new JButton("Button"));
-        mainPanel.setVisible(true);
-        
-        frame.add(mainPanel, BorderLayout.CENTER);
+        frame.setResizable(false);     
+     }
+    
+    public void startUp(){
+        Startup startup = new Startup();
+        frame.add(startup.getPanel(), BorderLayout.CENTER);
         frame.setVisible(true);
+    }
+    
+    public void startMenu(){
         
     }
     
